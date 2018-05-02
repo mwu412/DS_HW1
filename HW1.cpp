11e2fd601327177d171108ceda26d4536638b613
Michael Wu <38211512+mwu412@users.noreply.github.com>
2018-05-02 16:41:43 +0800

    Final Version

---
+++ HW1.cpp
@@ -0,0 +1,57 @@
+﻿//  Created by Mike3 on 20/03/2018.
+//  Copyright © 2018 Mike3. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <limits>
+using namespace std;
+
+string in;
+
+
+bool inspect(int d, int i, long last) {
+
+	if (d<0) return false;
+	else if (last <= i) return true;
+
+	else if (in[i] == in[last]) {
+
+		return (inspect(d, i + 1, last - 1));
+	}
+
+	else {
+		return (inspect(d - 1, i + 1, last)) ||
+			(inspect(d - 1, i, last - 1));
+	}
+}
+
+
+
+
+int main() {
+	int i = 0;
+	int d;
+	size_t len;//number of delete
+	long last;
+	bool result;
+
+	cin >> d;
+	cin.ignore();
+
+	//read line by line
+	while (getline(cin, in)) {
+		len = in.length();
+		last = len - 1;
+
+		result = inspect(d, i, last);
+
+
+		if (result) cout << "Yes" << '\n';
+		else cout << "No" << '\n';
+
+	}
+
+	system("pause");
+	return 0;
+}
